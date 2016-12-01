6c837b6b9b9af762553f3669aff12c1fdf8cac02
Malik Harrison <harrmalik@gmail.com>
2016-12-01 17:39:22 -0500

    basic application

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+let express = require('express');
+let router = express.Router();
+let _ = require('lodash');
+let request = require('request');
+let key = require('../keys');
+
+let champs = [];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    request(`https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=${key}`, function (error, response, body) {
+        let data = JSON.parse(body);
+        champs = _.map(data.champions, function(champ) {
+            return champ.id;
+        })
+        res.render('index', {
+            'title': 'Free this week'
+        });
+    });
+});
+
+router.get('/champs', function(req, res, next) {
+    request(`https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=tags&api_key=${key}`, function (error, response, body) {
+        let data = JSON.parse(body);
+
+        champions = _.map(data.data, function(champ) {
+            if (_.indexOf(champs, champ.id) > 0) {
+                return {
+                    id: champ.id,
+                    name: champ.name,
+                    title: champ.title,
+                    tags: champ.tags,
+                    image: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ.key}_0.jpg`,
+                    avatar: `http://ddragon.leagueoflegends.com/cdn/6.23.1/img/champion/${champ.key}.png`,
+                    sound: `/sounds/${champ.key}.mp3`
+                }
+            } else {
+                return '';
+            }
+        });
+        champions = _.filter(champions, 'id');
+        res.send(champions);
+    });
+});
+
+module.exports = router;
