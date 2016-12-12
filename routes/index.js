8b3e7e51a52753c15b38aa72edc915dcf7033f24
Malik Harrison <harrmalik@gmail.com>
2016-12-12 14:59:37 -0500

    changed key

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let _ = require('lodash');
 let request = require('request');
-let key = process.env.api-key;
+let key = process.env.apiKey;
 
 let champs = [];
 
